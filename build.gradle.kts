6a839697d96ba4177e88484fb27ff517f524471a
Kevin Nord <kevin@example.com>
2025-09-21 10:39:33 -0400

    Specify kapt plugin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.25" apply false
     id("com.google.dagger.hilt.android") version "2.52" apply false
-    id("kotlin-kapt") apply false
+    kotlin("kapt") version "1.9.25" apply false
 }
 
 tasks.register("clean", Delete::class) {
